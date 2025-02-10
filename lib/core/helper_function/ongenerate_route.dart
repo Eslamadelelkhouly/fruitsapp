951fac08d2be80c6dcffaabc3a4f36ca33fb76a5
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-11 00:37:30 +0200

    route on_boarding screen

--- lib/core/helper_function/ongenerate_route.dart
+++ lib/core/helper_function/ongenerate_route.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruitsapp/features/onBoarding/presentation/views/on_boarding_view.dart';
 import 'package:fruitsapp/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fruitsapp/features/onBoarding/presentation/views/on_boarding_view.dart';
 import 'package:fruitsapp/features/splash/presentation/views/widgets/splash_view_body.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
   static const String routeName = "splash";
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    RouteOnBoarding();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: const SplashViewBody()),
+    return const Scaffold(
+      body: SafeArea(child: SplashViewBody()),
+    );
+  }
+
+  void RouteOnBoarding() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+      },
     );
   }
 }
