5524124786d6ebd81350143348e5aebb3e09cf80
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-12 13:01:28 +0200

    add dots indicator

---
+++ lib/core/utils/constants/color_app.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  static const kPrimaryColor = Color(0xff1b5e37);
+}
--- lib/features/onBoarding/presentation/views/widgets/on_boarding_body.dart
+++ lib/features/onBoarding/presentation/views/widgets/on_boarding_body.dart
@@ -1,4 +1,6 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruitsapp/core/utils/constants/color_app.dart';
 import 'package:fruitsapp/features/onBoarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingBody extends StatelessWidget {
@@ -6,9 +8,18 @@ class OnBoardingBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Expanded(child: OnBoardingPageView()),
+        Expanded(
+          child: OnBoardingPageView(),
+        ),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: ColorApp.kPrimaryColor,
+            color: ColorApp.kPrimaryColor.withOpacity(.5),
+          ),
+        ),
       ],
     );
   }
