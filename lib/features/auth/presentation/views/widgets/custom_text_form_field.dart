780cf32bed819788375237178683bfd9de296d95
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-18 00:15:35 +0200

    add custom text from field

---
+++ lib/features/auth/presentation/views/widgets/custom_text_form_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fruitsapp/core/utils/constants/app_text_style.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({super.key, required this.text, this.suffixIcon});
+  final String text;
+  final Widget? suffixIcon;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 17),
+      child: TextFormField(
+        decoration: InputDecoration(
+          suffixIcon: suffixIcon,
+          hintText: text,
+          hintStyle: TextStyles.bold13,
+          filled: true,
+          fillColor: Color(0xffE6E9EA),
+          border: CustomBorder(),
+          enabledBorder: CustomBorder(),
+          disabledBorder: CustomBorder(),
+          focusedBorder: CustomBorder(),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder CustomBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(4),
+      borderSide: BorderSide(color: Color(0xffF9FAFA)),
+    );
+  }
+}
--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:fruitsapp/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        SizedBox(
+          height: 24,
+        ),
+        CustomTextFormField(
+          text: 'البريد الاكتروني',
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        CustomTextFormField(
+          text: 'كلمة المرور',
+          suffixIcon: Icon(
+            Icons.remove_red_eye,
+            color: Color(0xffC9CECF),
+          ),
+        ),
+      ],
+    );
   }
 }
