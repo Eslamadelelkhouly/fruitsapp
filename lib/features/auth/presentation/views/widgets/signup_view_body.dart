612193362431b3379b5b1e447faf5b98303d21c7
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-20 02:29:25 +0200

    add form field in sign up body

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:fruitsapp/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 17),
+        child: Column(
+          children: [
+            CustomTextFormField(
+              text: 'الاسم كامل',
+              keyboardType: TextInputType.name,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              text: 'البريد الإلكتروني',
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              text: 'كلمة المرور',
+              keyboardType: TextInputType.visiblePassword,
+              suffixIcon: Icon(
+                Icons.remove_red_eye,
+                color: Color(0xffC9CECF),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
