137697423185e39e1076807817a648e6fff4464e
Eslamadelelkhouly <rodyerak@gmail.com>
2025-03-08 19:33:35 +0200

    implment adduser method

--- lib/core/services/firestore_services.dart
+++ lib/core/services/firestore_services.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fruitsapp/core/services/database_services.dart';
 
 class FireStoreServices implements DatabaseServices {
-  @override
-  Future<void> addUser({required String path, required Map<String, dynamic> data}) {
-    // TODO: implement addUser
-    throw UnimplementedError();
-  }
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  @override
+  Future<void> addUser(
+      {required String path, required Map<String, dynamic> data}) async {
+    await firestore.collection(path).add(data);
+  }
 }
